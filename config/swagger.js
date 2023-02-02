2ef13ff10c9d037db9a3749023f98232a94c1d0f
kuldeep-vitto <kuldeep@vitto.money>
2023-02-02 18:50:22 +0530

    update 1 -v3

--- config/swagger.js
+++ config/swagger.js
@@ -3,7 +3,7 @@ import path from "path";
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const hostedUrl = process.env.ROOT_URL || 'http://localhost:5000/';
+const hostedUrl = process.env.ROOT_URL || 'https://ticket-booking-kw5w.onrender.com/';
 const options = {
   definition: {
     openapi: '3.0.0',
